13a6ae6001a25fdc4b44f27793d76e188960cc0d
Andrew Duncan <andrew.duncan@oicr.on.ca>
2020-01-23 13:27:41 -0500

    fix test

--- cypress/integration/ExploreGDC.js
+++ cypress/integration/ExploreGDC.js
@@ -120,7 +120,7 @@ describe('Explore GDC', function() {
         })
 
         checkAllResultsTab(
-            ['Showing 1 to 20 of 3,102', 'TCGA-A5-A1OF', 'TCGA-AJ-A3EK'],
+            ['Showing 1 to 20 of 3,107', 'TCGA-A5-A1OF', 'TCGA-AJ-A3EK'],
             ['Showing 1 to 20 of 21,168', 'TP53', 'TTN'],
             ['Showing 1 to 20 of 120,824', 'chr2:g.208248388C>T', 'chr17:g.7673803G>A']
         )
