da2ed4e510d20cfbb9facf724d2c805c2d56d267
Gregory Hogue <gfjhogue@gmail.com>
2020-08-05 12:35:12 -0400

    add SeqFeature store class for Gene Level Copy Number Track

---
+++ gdc-viewer/js/Store/SeqFeature/GeneLevelCopyNumber.js
@@ -0,0 +1,33 @@
+/**
+ * GeneLevelCopyNumber store class
+ * Supports (Data format, Data category, Data Type)
+ * * TXT, Copy Number Variation, Gene Level Copy Number
+ */
+define([
+    'dojo/_base/declare',
+    './BaseBEDLikeFeature',
+],
+function (
+    declare,
+    BaseBEDLikeFeature
+) {
+    return declare(BaseBEDLikeFeature, {
+        convertLineToBED: function(line) {
+            originalLine = line.split('\t');
+
+            bedLikeLine = [originalLine[2].replace('chr', '')];  // chr
+            bedLikeLine.push(originalLine[3]);  // start
+            bedLikeLine.push(originalLine[4]);  // end
+            bedLikeLine.push(originalLine[1]);  // name
+            bedLikeLine.push(originalLine[5]);  // score
+            bedLikeLine.push(null);  // strand
+
+            fullLine = bedLikeLine.concat(originalLine);
+            return fullLine.join('\t');
+        },
+
+        getName: function() {
+            return 'GeneLevelCopyNumber'
+        }
+    })
+})
