38c101b7249bdfb7bd04e21d47fc6bbd78a27b41
Andrew Duncan <andrew.duncan@oicr.on.ca>
2020-08-06 10:49:40 -0400

    removed comment

--- gdc-viewer/js/Store/SeqFeature/MethylationBetaValue.js
+++ gdc-viewer/js/Store/SeqFeature/MethylationBetaValue.js
@@ -12,8 +12,6 @@ function (
     BaseBEDLikeFeature
 ) {
     return declare(BaseBEDLikeFeature, {
-        // Composite Element REF	Beta_value	Chromosome	Start	End	Gene_Symbol	Gene_Type	Transcript_ID	Position_to_TSCGI_Coordinate	Feature_Type
-        // cg00000292	0.713849646891845	chr16	28878779	28878780	ATP2A1;ATP2A1;ATP2A1;ATP2A1;ATP2A1	protein_coding;protein_coding;protein_coding;protein_coding;protein_coding	ENST00000357084.6;ENST00000395503.7;ENST00000536376.4;ENST00000562185.4;ENST00000563975.1	373;290;-1275;-465;-83	CGI:chr16:28879633-28880547	N_Shore
         convertLineToBED: function(line) {
             originalLine = line.split('\t');
 
